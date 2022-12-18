c8462c1a910d063ef834181c488e0f7f2f915938
Anatoly S <anatoly@LAPTOP-07LU5AD5.localdomain>
2022-12-19 00:28:14 +0300

    created views and url templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,5 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at the question {question_id}")
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question {id}"
+    return HttpResponse(response.format(id=question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
+
+
