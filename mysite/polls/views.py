b56079571440e24b79692475501e4e90a30e0a33
Anatoly S <anatoly@LAPTOP-07LU5AD5.localdomain>
2022-12-19 06:38:36 +0300

    add result template and changed views and detail.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -18,9 +19,21 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question {id}"
-    return HttpResponse(response.format(id=question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',
+                      {
+                          'question': question,
+                          'error_message': "You didn't select a choice.",
+                      })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
